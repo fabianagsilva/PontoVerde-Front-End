327860a8dc4e79e5b810ff2b8543d59b9863d041
fabianagsilva <bisilvag2@gmail.com>
2022-11-03 20:24:11 -0300

    @fabianagsilva [Task 1 - Criação Cadastro.tsx e Cadastro.css] 03.11.2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Home from "./paginas/home/Home"
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
+import Cadastro from "./paginas/cadastrar/Cadastrar";
+import Cadastrar from "./paginas/cadastrar/Cadastrar";
 
 function App() {
-
   return (
-  <h1>Projeto Ponto Verde</h1>
-  );
-
+    <>
+    <Home />
+    <Cadastrar />
+    </>
+  )
 }
 
 export default App;
---
+++ src/paginas/cadastrar/Cadastrar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import './Cadastrar.css';
+
+
+function Cadastrar() {
+    return (
+    <>
+    <h1>teste</h1>
+    </>
+    )
+}
+
+export default Cadastrar;
