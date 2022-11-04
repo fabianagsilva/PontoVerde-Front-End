b38b6815ade91277dcfb7bdb047d2ab3b770d2aa
flrvia <flavixbatista@gmail.com>
2022-11-03 22:45:20 -0300

    @thaissevero [Adicionando a Navbar do projeto] 03.11.2022

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
 import Cadastrar from "./paginas/cadastrar/Cadastrar";
-// import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/statics/navbar/Navbar";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <>
-    <Home />
-    <Login />
-    <Cadastrar />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />}/>
+          <Route path='/home' element={<Home />}/>
+          <Route path='/cadastrar' element={<Cadastrar />}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/statics/navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Navbar.css';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box style={{ cursor: 'pointer' }}>
+                        <Typography variant="h5" color="inherit"> Ponto Verde. </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} style={{ cursor: 'pointer' }}>
+                            <Typography variant="h6" color="inherit"> Inicio </Typography>
+                        </Box>
+
+                        <Box mx={1} style={{ cursor: 'pointer' }}>
+                            <Typography variant="h6" color="inherit"> Nosso Objetivo </Typography>
+                        </Box>
+
+                        <Box mx={1} style={{ cursor: 'pointer' }}>
+                            <Typography variant="h6" color="inherit"> Temas </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: 'pointer' }}>
+                            <Typography variant="h6" color="inherit"> Cadastrar Temas </Typography>
+                        </Box>
+
+                        <Box mx={1} style={{ cursor: 'pointer' }}>
+                            <Typography variant="h6" color="inherit"> Fazer Login </Typography>
+                            </Box>
+
+                        <Box mx={1} style={{ cursor: 'pointer' }}>
+                            <Typography variant="h6" color="inherit"> Encerrar Sessão </Typography>
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
