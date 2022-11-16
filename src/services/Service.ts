74c253647a6e8ba93eedd6cc657b20e1ad017604
thaissevero <thais.severo21@gmail.com>
2022-11-16 15:20:31 -0300

    @thaissevero [Task 10: Service] 16.11.2022

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,3 +1,5 @@
+// @thaissevero on github
+
 import axios from 'axios';
 
 export const api = axios.create({
@@ -12,4 +14,28 @@ export const api = axios.create({
     export const login = async (url: any, dados: any, setDados: any) => {
         const resposta = await api.post(url, dados);
         setDados(resposta.data.token);
+    }
+
+    export const busca = async (url: any, setDados:any, header: any) => {
+        const resposta = await api.get(url, header)
+        setDados(resposta.data)
+    }
+
+    export const buscaId = async (url: any, setDados: any, header: any) => {
+        const resposta = await api.get(url, header)
+        setDados(resposta.data)
+    }
+
+    export const post = async (url: any, dados: any, setDados: any, header: any) => {
+        const resposta = await api.post(url, dados, header)
+        setDados(resposta.data)
+    }
+
+    export const put = async (url: any, dados: any, setDados: any, header: any) => {
+        const resposta = await api.put(url, dados, header)
+        setDados(resposta.data)
+    }
+
+    export const deleteId = async (url: any, header: any) => {
+        await api.delete(url, header)
     }
