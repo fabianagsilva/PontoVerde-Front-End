b05bad424e067e2871df7a185e3a85ee22e14c41
AnaPaulaNunes <apnuness29@gmail.com>
2022-11-16 19:40:46 -0300

    @AnaPaulaNunes [Atualização redux navbar e footer] - 16.11.2022

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHub from '@mui/icons-material/GitHub';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { Typography, Box, Grid } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import "./Footer.css"
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    var footerComponent;
+
+    if (token !== "") {
+        footerComponent = 
+        <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
                     <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
@@ -15,7 +25,7 @@ function Footer() {
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/flrvia/PontoVerde-Front-End" target="_blank">
-                                <GitHub style={{ fontSize: 60, color: "white" }} />
+                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
                             </a>
                             <a href="https://www.instagram.com/generationbrasil/" target="_blank">
                                 <InstagramIcon style={{ fontSize: 60, color: "white" }} />
@@ -37,6 +47,11 @@ function Footer() {
                     </Box>
                 </Grid>
             </Grid>
+    }
+    
+    return (
+        <>
+        {footerComponent}
         </>
     )
 }
--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -2,38 +2,90 @@ import React from 'react';
 import './Navbar.css';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { addToken } from '../../../store/tokens/actions';
+import { toast } from 'react-toastify';
 
-export default function Navbar() {
+function Navbar() {
+
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    )
+
+    function goLogout() {
+        dispatch(addToken(""))
+        toast.info("Usuário deslogado!", {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        })
+        navigate("/login")
+    }
+
+    var navbarComponent;
+
+    if(token !== "") {
+        navbarComponent =
+        <AppBar position="static">
+        <Toolbar variant="dense">
+            <Box style={{ cursor: 'pointer' }}>
+                <Typography variant="h5" color="inherit"> Ponto Verde. </Typography>
+            </Box>
+
+            <Box display="flex" justifyContent="start">
+                <Link to="/home" className= "text-decorator-none">
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit"> Home </Typography>
+                </Box>
+                </Link>
+
+                <Link to="/quemSomos" className="text-decorator-none">
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit"> Quem somos </Typography>
+                </Box>
+                </Link>
+
+                <Link to="/categoria" className="text-decorator-none">
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit"> Categorias </Typography>
+                </Box>
+                </Link>
+
+                <Link to="/formularioCategoria" className="text-decorator-none">
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit"> Cadastrar categoria </Typography>
+                </Box>
+                </Link>
+
+                <Link to="/postagens" className="text-decorator-none">
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit"> Postagens </Typography>
+                </Box>
+                </Link>
+
+                <Box mx={1} className= "cursor" onClick= {goLogout}>
+                    <Typography variant="h6" color="inherit"> Encerrar sessão </Typography>
+                </Box>
+                
+            </Box>
+        </Toolbar>
+    </AppBar>
+    }
+    
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box style={{ cursor: 'pointer' }}>
-                        <Typography variant="h5" color="inherit"> Ponto Verde. </Typography>
-                    </Box>
-
-                    <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: 'pointer' }}>
-                            <Typography variant="h6" color="inherit"> Home </Typography>
-                        </Box>
-
-                        <Box mx={1} style={{ cursor: 'pointer' }}>
-                            <Typography variant="h6" color="inherit"> Nosso objetivo </Typography>
-                        </Box>
-
-                        <Box mx={1} style={{ cursor: 'pointer' }}>
-                            <Typography variant="h6" color="inherit"> Categorias </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: 'pointer' }}>
-                            <Typography variant="h6" color="inherit"> Cadastrar categoria </Typography>
-                        </Box>
-
-                        <Box mx={1} style={{ cursor: 'pointer' }}>
-                            <Typography variant="h6" color="inherit"> Encerrar sessão </Typography>
-                        </Box>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+        {navbarComponent}
         </>
     );
-}
+};
+
+export default Navbar;
