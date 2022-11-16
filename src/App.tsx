d74da5f32cfa57494a973bb239008791049a975e
fabianagsilva <bisilvag2@gmail.com>
2022-11-16 12:09:48 -0300

    @fabianagsilva - Task PI, Adicionado Store e Redux - 16.11.22

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
-import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/statics/footer/Footer";
+import Navbar from "./components/statics/navbar/Navbar";
+import CadastroUsuario from "./paginas/cadastrar/CadastroUsuario";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
-import Navbar from "./components/statics/navbar/Navbar";
-import Footer from "./components/statics/footer/Footer";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import "./App.css";
-import CadastroUsuario from "./paginas/cadastrar/CadastroUsuario";
+import store from "./Store/store";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      
-      <div style={{ minHeight: "100vh" }}>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path="/cadastrar" element={<CadastroUsuario/>} />
-        </Routes>
-      </div>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
 
-      <Footer />
-      
-    </Router>
-    
+        <div style={{ minHeight: "100vh" }}>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path="/cadastrar" element={<CadastroUsuario />} />
+          </Routes>
+        </div>
+
+        <Footer />
+
+      </Router>
+    </Provider>
   );
 }
 
