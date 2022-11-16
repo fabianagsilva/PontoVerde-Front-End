bde70b7b7ea4fe1eb352a79d0874fa6aafb08669
Flavia Santana <65964287+flrvia@users.noreply.github.com>
2022-11-16 17:25:33 -0300

    Update Postagem.ts

--- src/models/Postagem.ts
+++ src/models/Postagem.ts
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-import categoria from './categoria'
-=======
 
 import Categoria from './Categoria';
 import User from './User';
->>>>>>> ba565fe4dea457fd9b076609c3cf6dbde01e3278
+
 
 interface Postagem{
     id: number;
     titulo: string;
     subtitulo: string;
     texto: string;
-<<<<<<< HEAD
-    categoria?: categoria| null
-=======
     imagem: string;
     autor: string;
     data: string;
@@ -21,7 +15,6 @@ interface Postagem{
     sugestao: string;
     categoria?: Categoria| null;
     usuario?: User| null;
->>>>>>> ba565fe4dea457fd9b076609c3cf6dbde01e3278
 }
 
 export default Postagem;
