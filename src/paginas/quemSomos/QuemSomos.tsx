613116e9341ae42e460f5054a3532e56b0b1f974
flrvia <flavixbatista@gmail.com>
2022-11-21 12:55:02 -0300

    @flrvia [Estilização] 21.11.2022

--- src/paginas/quemSomos/QuemSomos.tsx
+++ src/paginas/quemSomos/QuemSomos.tsx
@@ -1,30 +1,165 @@
 import "./QuemSomos.css";
-import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
-import { Box } from '@mui/material';
+import React from "react";
+import { Grid, Paper } from "@material-ui/core";
+import { Box } from "@mui/material";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function QuemSomos() {
-    return (
-        <>
-            <Paper>
-                <Grid alignItems='center' xs={6}>
-                    <Box p={2}>
-                        <Box display="flex" justifyContent='center'>
-                            <h1>Quem Somos</h1>
-                        </Box>
-                        <Box display="flex" justifyContent='center' p={2}>
-                            <h1><img src="https://i.imgur.com/WYzfxmf.png" alt="" style={{ width: '100%', height: '100%' }} /></h1>
-                        </Box>
-                        <Box>
-                            <h4>Objetivo</h4>
-                            <p>Desmistificar sobre o que é sustentabilidade para a periferia, promovendo a conscientização das atuais e futuras gerações.</p>
-                            <p>Queremos que a população tenha um pensamento sustentável e explore técnicas e materiais que possam ser usados para preservar os recursos naturais em suas comunidades.</p>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Paper>
-        </>
-    );
+  return (
+    <>
+      <Paper className="tela">
+        <Grid alignItems="center" xs={12}>
+          <Box p={2}>
+            <Box display="flex" justifyContent="center">
+              <h1>Quem Somos</h1>
+            </Box>
+          </Box>
+          <Box className="cardsIntegrantes">
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="img"></span>
+                <h2>
+                  Leticia<br></br>
+                  <span>Silva</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="https://www.linkedin.com/in/silva-leticia/">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/lettycodes">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="imgflavia"></span>
+                <h2>
+                  Flávia<br></br>
+                  <span>Santana</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="https://www.linkedin.com/in/flaviasantanab/">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/flrvia">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="imga"></span>
+                <h2>
+                  Ana Paula<br></br>
+                  <span>Nunes</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="https://www.linkedin.com/in/ana-paula-nunes-/">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/AnaPaulaNunes">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="imgm"></span>
+                <h2>
+                  Mari<br></br>
+                  <span>Rebecca</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/marirebecca">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="imgt"></span>
+                <h2>
+                  Thais<br></br>
+                  <span>Severo</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="https://www.linkedin.com/in/thais-severo/">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/thaissevero">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="imgf"></span>
+                <h2>
+                  Fabiana<br></br>
+                  <span>Gonçalves</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="https://www.linkedin.com/in/fabianagoncalvessilva/">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/fabianagsilva">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+
+            <Box className="cards">
+              <div className="card">
+                <div className="blob"></div>
+                <span className="imgl"></span>
+                <h2>
+                  Lidiane<br></br>
+                  <span>Angelo</span>
+                </h2>
+                <h6>Desenvolvedora Full Stack Junior</h6>
+                <p>
+                  <a href="https://www.linkedin.com/in/lidianeangelo/">
+                    <LinkedInIcon className="icon" />
+                  </a>
+                  <a href="https://github.com/Lidianeangelo">
+                    <GitHubIcon className="icon2" />
+                  </a>
+                </p>
+              </div>
+            </Box>
+          </Box>
+        </Grid>
+      </Paper>
+    </>
+  );
 }
 
 export default QuemSomos;
